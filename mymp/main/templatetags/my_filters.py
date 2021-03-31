15841352b90cdfaef1710b19d39e9100f70d41da
realzuy <zuy@zuy.ru>
2021-03-31 20:25:56 +0300

    3.4

--- mymp/main/templatetags/my_filters.py
+++ mymp/main/templatetags/my_filters.py
@@ -12,3 +12,7 @@ def my_text_reverse(value):
 @register.simple_tag
 def current_time(format_string):
     return datetime.datetime.now().strftime(format_string)
+
+@register.simple_tag
+def get_verbose_field_name(instance, field_name):
+    return instance._meta.get_field(field_name).verbose_name.title()
--- mymp/main/urls.py
+++ mymp/main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
 from . import views
+from .views import StrategyList, StrategyDetail
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('strategies/', StrategyList.as_view()),
+    path('strategies/<int:pk>/', StrategyDetail.as_view(), name='strategy-detail'),
 ]
