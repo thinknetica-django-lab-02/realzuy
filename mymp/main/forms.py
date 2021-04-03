402f81e075e1cb1acdd5c9f69360de274752bb98
realzuy <zuy@zuy.ru>
2021-04-03 21:08:25 +0300

    другой виджет для даты + валидация

--- mymp/main/forms.py
+++ mymp/main/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from dateutil.relativedelta import relativedelta
 from datetime import datetime
+
 from .models import Profile
 
 
@@ -18,7 +19,8 @@ class ProfileForm(forms.ModelForm):
         fields = ['birth_date']
 
 
-class ProfileFormset(forms.inlineformset_factory(User, Profile, fields='__all__', can_delete=False)):
+class ProfileFormset(forms.inlineformset_factory(User, Profile, fields='__all__', can_delete=False, widgets={'birth_date': forms.SelectDateWidget(years=range(1920,2020)),})):
+
     def __init__(self, *args, **kwargs):
         self.__initial = kwargs.pop('initial', [])
         super(ProfileFormset, self).__init__(*args, **kwargs)
@@ -27,7 +29,11 @@ class ProfileFormset(forms.inlineformset_factory(User, Profile, fields='__all__'
         super(ProfileFormset, self).clean()
 
         for form in self.forms:
-            birth_date = form.cleaned_data['birth_date']
+            try:
+                birth_date = form.cleaned_data['birth_date']
+            except Exception:
+                raise forms.ValidationError('')
+
             if relativedelta(datetime.now(), birth_date).years < 18:
                 raise ValidationError('Возвраст должен быть больше 18 лет')
 
