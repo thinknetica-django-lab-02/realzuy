556d691c8331aaa5b0f2c2bb30ad5d8552018a8a
realzuy <zuy@zuy.ru>
2021-04-10 21:20:00 +0300

    6.4

--- mymp/main/schedule.py
+++ mymp/main/schedule.py
@@ -9,13 +9,11 @@ from .models import Strategy, Profile
 scheduler = BackgroundScheduler()
 
 def send_new_strategies_weekly_schedule():
-    print('1')
     date_start = datetime.now() - timedelta(days=7)
     strategies = Strategy.objects.filter(date_create__gte=date_start)
 
     for profile in Profile.objects.all():
         if profile.subscriptions.filter(id=1):
-            print('2')
             SendNewStrategiesMessage(strategies, profile)
 
 trigger = CronTrigger(day_of_week='mon', hour=10)
