ed8108913e9db3af4d498da0fb255a50f3b36350
realzuy <zuy@zuy.ru>
2021-04-18 15:44:55 +0300

    8.3

--- mymp/main/urls.py
+++ mymp/main/urls.py
@@ -3,11 +3,15 @@ from .views import *
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+from django.views.decorators.cache import cache_page
+
+CACHE_MIDDLEWARE_SECONDS = getattr(settings, 'CACHE_MIDDLEWARE_SECONDS', DEFAULT_TIMEOUT)
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('strategies/', StrategyList.as_view(), name='strategies'),
-    path('strategies/<int:pk>/', StrategyDetail.as_view(), name='strategy-detail'),
+    path('strategies/<int:pk>/', cache_page(CACHE_MIDDLEWARE_SECONDS)(StrategyDetail.as_view()), name='strategy-detail'),
     path('accounts/profile/', views.update_profile, name='profile-update'),
     path('accounts/profile/phone_confirm', views.phone_number_confirmation, name='phone-confirm'),
     path('strategies/add/', StrategyCreate.as_view(), name='strategy-form'),
