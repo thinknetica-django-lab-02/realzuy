dc5d8b50532866be4ade4b6f11cd15e3ef47d731
realzuy <zuy@zuy.ru>
2021-04-27 19:41:52 +0300

    11.3 с проблемой

---
+++ mymp/main/management/commands/gentestdata.py
@@ -0,0 +1,65 @@
+from django.core.management.base import BaseCommand, CommandError
+from factory import SubFactory, Iterator
+from factory.django import DjangoModelFactory
+from main.models import Strategy, StrategyCategory, StrategyAuthor
+from datetime import datetime
+from faker import Factory
+
+faker = Factory.create()
+today = datetime.now()
+
+class StrategyCategoryFactory(DjangoModelFactory):
+    class Meta:
+        model = StrategyCategory
+        django_get_or_create = ('name',)
+
+    name = faker.word()
+
+class StrategyAuthorFactory(DjangoModelFactory):
+    class Meta:
+        model = StrategyAuthor
+        django_get_or_create = ('email',)
+
+    first_name = faker.name()
+    last_name = faker.name()
+    email = faker.email()
+
+class StrategyFactory(DjangoModelFactory):
+    class Meta:
+        model = Strategy
+
+    title = faker.word(),
+    date_create = today,
+    date_modify = today,
+    #ни один из вариантов создания категории и автора не заработал
+    #ошибка:CommandError: error: Cannot assign "(<StrategyCategory: Внутридневная>,)": "Strategy.id_category" must be a "StrategyCategory" instance.
+    #id_category = SubFactory(StrategyCategoryFactory, name='Внутридневная'),
+    #id_category = StrategyCategory.objects.get(id=1),
+    #id_category = StrategyCategoryFactory(name='Внутридневная'),
+    id_category = Iterator(StrategyCategory.objects.all()),
+    id_author = SubFactory(StrategyAuthorFactory, email='a@a.ru'),
+    min_nav = faker.random_int(),
+    annual_return = faker.random_int()
+
+
+class Command(BaseCommand):
+    help = 'Генерация тестовых данных для модели'
+
+    def handle(self, *args, **options):
+        try:
+            #создание объекта не из класса DjangoModelFactory работает
+            strategy = Strategy.objects.create(
+                title=faker.word(),
+                date_create=today,
+                date_modify=today,
+                id_category=StrategyCategoryFactory(name='Внутридневная'),
+                id_author=StrategyAuthorFactory(email='a@a.ru'),
+                min_nav=faker.random_int(),
+                annual_return=faker.random_int()
+            )
+            self.stdout.write(self.style.SUCCESS('strategy created: ' + strategy.title))
+
+            strategy = StrategyFactory()
+            self.stdout.write(self.style.SUCCESS('strategy created: ' + strategy.title))
+        except Exception as e:
+            raise CommandError('error: ' + str(e))
